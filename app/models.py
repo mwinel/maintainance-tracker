66f9fff4af7ffbfb082acc117b25136d17e71b1e
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-07-14 17:35:34 +0300

    Add user class

---
+++ app/models.py
@@ -0,0 +1,36 @@
+# Create classes to store data.
+
+class User:
+	""" This class provides a way to store user data. """
+
+	users = [
+		{
+			'id': 1,
+			'username': u'mwinel',
+			'email': u'mwinel@example.com',
+			'password': u'123456'
+		},
+		{
+			'id': 2,
+			'username': u'lucy',
+			'email': u'lucy@example.com',
+			'password': u'123456'
+		}
+	]
+
+	def __init__(self, id, username, email, password):
+		""" Initialize objects. """
+		self.id = user_id,
+		self.username = username,
+		self.email = email,
+		self.password = password
+
+	def getUsers():
+		""" Method to return a list of users. """
+		return User.users, 200
+
+	def getUser(id):
+		user = [user for user in User.users if user['id'] == id]
+		if not user:
+			return {'message': 'User not found'}
+		return {'user': user[0]}, 200
