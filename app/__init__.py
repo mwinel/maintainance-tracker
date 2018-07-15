16389f6cdece114d74b5c646affccdbe36fb3bff
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-07-15 11:37:03 +0300

    Add jwt secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from config import app_config
 
+
+jwt = JWTManager()
 
 def create_app(config_class):
 	app = Flask(__name__)
+	app.config.from_object(app_config[config_class])
+	app.config['JWT_SECRET_KEY'] = "jwt-secret-string"
 	api = Api(app)
+	jwt = JWTManager(app)
 
 	return app
 
