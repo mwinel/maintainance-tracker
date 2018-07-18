7f47be59e534ccb41c0e7e7762b60d36c700cc45
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-07-18 17:54:56 +0300

    Check if token in blacklist

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,18 @@ def create_app(config_class):
 	app = Flask(__name__)
 	app.config.from_object(app_config[config_class])
 	app.config['JWT_SECRET_KEY'] = "tPXJY3X37Qybz4QykV+hOyUxVQeEXf1Ao2C8upz+fGQXKsM"
+	app.config['JWT_BLACKLIST_ENABLED'] = True
+	app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 	api = Api(app)
 	jwt = JWTManager(app)
+
+	# Callback everytime a secured endpoint is accessed.
+	# Return True if token is blacklisted.
+	# Return False if token is not blacklisted.
+	@jwt.token_in_blacklist_loader
+	def check_if_token_in_blacklist(decrypted_token):
+		jti = decrypted_token['jti']
+		return models.RevokedToken.is_jti_blacklisted(jti)
 
 	return app
 
